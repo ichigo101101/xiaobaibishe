56c2943a956d250804b13d4924e2e56343e3f6e4
ichigo101101 <1429553410@qq.com>
2024-07-08 18:35:43 +0900

    first

---
+++ vue/src/views/AdminView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="about">
+    <div>
+      <el-input style="width: 200px; margin-right: 10px" placeholder="请输入内容"></el-input>
+      <el-button type="warning">搜索</el-button>
+      <el-button type="primary">新增</el-button>
+    </div>
+    <div>
+      <el-table :data="tableData" style="width: 100%; margin: 15px 0px">
+        <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column label="操作">
+          <el-button type="primary">编辑</el-button>
+          <el-button type="danger">删除</el-button>
+        </el-table-column>
+      </el-table>
+      <div class="block">
+        <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage4"
+            :page-sizes="[10, 20, 30, 40]"
+            :page-size="10"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="400">
+        </el-pagination>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+}
+</script>
+
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div style="font-size: 18px">23年9月23日开始做</div>
+    <div style="font-size: 18px; color: red; margin: 10px 0">好麻烦啊啊啊啊</div>
+    <div style="font-size: 18px">over</div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+}
+</script>
+
+
